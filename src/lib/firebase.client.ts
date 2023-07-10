446c2a840e2163f433160506859524bbebe35d3f
MrValk <n.s.peters05@gmail.com>
2023-07-10 15:00:12 +0200

    Improved names of env vars.

--- src/lib/firebase.client.ts
+++ src/lib/firebase.client.ts
@@ -4,13 +4,13 @@ import { collection, getFirestore } from 'firebase/firestore';
 import { GoogleAuthProvider, getAuth, browserLocalPersistence } from 'firebase/auth';
 
 import {
-	PUBLIC_FIREBASE_APIKEY,
-	PUBLIC_FIREBASE_AUTHDOMAIN,
-	PUBLIC_FIREBASE_PROJECTID,
-	PUBLIC_FIREBASE_STORAGEBUCKET,
-	PUBLIC_FIREBASE_MESSAGINGSENDERID,
-	PUBLIC_FIREBASE_APPID,
-	PUBLIC_FIREBASE_MEASUREMENTID
+	PUBLIC_FIREBASE_API_KEY,
+	PUBLIC_FIREBASE_AUTH_DOMAIN,
+	PUBLIC_FIREBASE_PROJECT_ID,
+	PUBLIC_FIREBASE_STORAGE_BUCKET,
+	PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	PUBLIC_FIREBASE_APP_ID,
+	PUBLIC_FIREBASE_MEASUREMENT_ID
 } from '$env/static/public';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,13 +18,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: PUBLIC_FIREBASE_APIKEY,
-	authDomain: PUBLIC_FIREBASE_AUTHDOMAIN,
-	projectId: PUBLIC_FIREBASE_PROJECTID,
-	storageBucket: PUBLIC_FIREBASE_STORAGEBUCKET,
-	messagingSenderId: PUBLIC_FIREBASE_MESSAGINGSENDERID,
-	appId: PUBLIC_FIREBASE_APPID,
-	measurementId: PUBLIC_FIREBASE_MEASUREMENTID
+	apiKey: PUBLIC_FIREBASE_API_KEY,
+	authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: PUBLIC_FIREBASE_APP_ID,
+	measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
