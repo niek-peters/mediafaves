ac732c170617fe2c592d5839e50cf3216208038c
MrValk <n.s.peters05@gmail.com>
2023-07-18 16:49:16 +0200

    Removed /.vercel dir from git repo

--- .vercel/output/static/_app/immutable/chunks/windowWidth.bd7fe143.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./singletons.df133e99.js";import{aP as n}from"./user.2e12a8a7.js";const e=t(),o=t(null);e.subscribe(r=>{o.set(s(r??0))});function s(r){return r<640?n.xs:r<768?n.sm:r<1024?n.md:r<1280?n.lg:r<1536?n.xl:n["2xl"]}export{o as b,e as w};
